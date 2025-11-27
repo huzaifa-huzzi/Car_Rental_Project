9a9cb47c3ad345ac1d519c6591ea05899e9724a1
huzaifa-huzzi <huzaifaatta8152@gmail.com>
2025-11-27 21:07:17 +0500

    Project setup

--- lib/Resources/Colors.dart
+++ lib/Resources/Colors.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color backgroundOfScreenColor = Colors.white;
   static const Color sideBarBackgroundColor = Colors.white;
-  static const Color primaryColor = Colors.white;
+  static const Color primaryColor = Color(0xFFFF2741);
+  static const Color secondaryColor = Color(0xFFECF5FA);
+  static const Color textColor = Color(0xFF1F3045);
+  static const Color secondTextColor = Color(0xFF4C596A);
+  static const Color tertiaryTextColor = Color(0xFFBCC1C7);
+  static const Color quadrantalTextColor = Color(0xFF868F9B);
 
 
 
---
+++ lib/Resources/IconStrings.dart
@@ -0,0 +1,18 @@
+
+
+class IconString {
+
+    /// SideBar Icons
+  static const String  dashboardIcon = 'assets/icons/dashboardIcon.png';
+  static const String  carInventoryIcon = 'assets/icons/CarInventory.png';
+  static const String  customerIcon = 'assets/icons/Customer.png';
+  static const String  agreementIcon = 'assets/icons/Agreement.png';
+  static const String  expensesIcon = 'assets/icons/Expenses.png';
+  static const String  incomeIcon = 'assets/icons/Income.png';
+  static const String  maintenanceIcon = 'assets/icons/maintenance.png';
+  static const String  paymentIcon = 'assets/icons/payment.png';
+
+
+
+
+}
