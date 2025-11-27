e594cfd1d1bf2a0169978fae5dacba7aedf7f065
huzaifa-huzzi <huzaifaatta8152@gmail.com>
2025-11-27 21:33:55 +0500

    Project setup

--- lib/Resources/Colors.dart
+++ lib/Resources/Colors.dart
@@ -3,12 +3,16 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color backgroundOfScreenColor = Colors.white;
   static const Color sideBarBackgroundColor = Colors.white;
+  static const Color blackColor = Colors.black;
   static const Color primaryColor = Color(0xFFFF2741);
   static const Color secondaryColor = Color(0xFFECF5FA);
-  static const Color textColor = Color(0xFF1F3045);
-  static const Color secondTextColor = Color(0xFF4C596A);
-  static const Color tertiaryTextColor = Color(0xFFBCC1C7);
-  static const Color quadrantalTextColor = Color(0xFF868F9B);
+  static const Color textColor = Color(0xFF1F3045); // Dark-blue
+  static const Color secondTextColor = Color(0xFF4C596A); // grey 30
+  static const Color tertiaryTextColor = Color(0xFFBCC1C7); // grey 10
+  static const Color quadrantalTextColor = Color(0xFF868F9B); // Grey 20
+  static const Color availableBackgroundColor = Color(0xFF3A62F3);
+  static const Color maintenanceBackgroundColor = Color(0xFFFF7F8F);
+  static const Color iconsBackgroundColor = Color(0xFFFFDADF);
 
 
 
--- lib/Resources/IconStrings.dart
+++ lib/Resources/IconStrings.dart
@@ -11,6 +11,12 @@ class IconString {
   static const String  incomeIcon = 'assets/icons/Income.png';
   static const String  maintenanceIcon = 'assets/icons/maintenance.png';
   static const String  paymentIcon = 'assets/icons/payment.png';
+  static const String  logoutIcon = 'assets/icons/logout.png';
+
+  /// AppBar Icons
+  static const String  searchIcon = 'assets/icons/Search.png';
+  static const String  settingIcon = 'assets/icons/Settings.png';
+  static const String  notificationIcon = 'assets/icons/NotificationIcon.png';
 
 
 
