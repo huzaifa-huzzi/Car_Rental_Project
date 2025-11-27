6bae0306747cb9f1f53e72a4f1ea184488520ba7
huzaifa-huzzi <huzaifaatta8152@gmail.com>
2025-11-27 19:53:03 +0500

    Initial commit

---
+++ lib/Resources/Colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundOfScreenColor = Colors.white;
+  static const Color sideBarBackgroundColor = Colors.white;
+  static const Color primaryColor = Colors.white;
+
+
+
+}
